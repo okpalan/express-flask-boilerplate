a3df42b4260d9f72d88d5445f50c6613f0c6c7f0
okpalan <89138089+okpalan@users.noreply.github.com>
2021-11-01 13:16:47 +0000

    Exit

--- noexceptions/server/bin/www.js
+++ noexceptions/server/bin/www.js
@@ -3,9 +3,21 @@ const https = require("https");
 const fs = require("fs");
 const path = require("path");
 const { spawn } = require("child_process");
+const PATH2JOIN = path.join("config", "ssl");
+const options = {
+  https: {
+    cert: fs.readFileSync(path.resolve(PATH2JOIN, "cert.pem")).toString(),
+    key: fs.readFileSync(path.resolve(PATH2JOIN, "key.pem")).toString(),
+    ca: fs.readFileSync(path.resolve(PATH2JOIN, "certreq.csr")).toString(),
+    requestCert: true,
+    rejectUnauthorized: false,
+  },
+};
+
 const io = require("socket.io")(https, {
   origins: [app.get("URL"), app.get("DOMAIN_URL")],
-});
+  ...options.https,
+}).listen(app.get("PORT"));
 
 io.on("connection", function (socket) {
   console.log("A user connected");
@@ -18,15 +30,6 @@ io.on("connection", function (socket) {
     console.log("A user disconnected");
   });
 });
-
-const PATH2JOIN = path.join("config", "ssl");
-const options = {
-  https: {
-    cert: fs.readFileSync(path.resolve(PATH2JOIN, "cert.pem")).toString(),
-    key: fs.readFileSync(path.resolve(PATH2JOIN, "key.pem")).toString(),
-    ca: fs.readFileSync(path.resolve(PATH2JOIN, "certreq.csr")).toString(),
-  },
-};
 
 const server = https
   .createServer(options.https, app)
